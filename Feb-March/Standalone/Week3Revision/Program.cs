7b0a950c6776e882ebb1d643c4a59b5dc3009897
ErrorDodo <41908416+ErrorDodo@users.noreply.github.com>
2024-02-18 13:03:49 +0800

    Add the wk3 revision

---
+++ Feb-March/Standalone/Week3Revision/Program.cs
@@ -0,0 +1,7 @@
+﻿var question = "What is your name?";
+Console.WriteLine(question);
+var name = Console.ReadLine();
+
+var qnA = $"{question}\n{name}";
+
+File.AppendAllText("/home/dylan/Documents/QnA.txt", qnA);
